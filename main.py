e7ad691eae59d4b464acd7d494f196aa862c826e
invasion <abdo.mongytrad@gmail.com>
2021-12-10 11:15:19 +0200

    buttons

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ class MainApp(QMainWindow, ui):
         super(MainApp, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.handle_buttons()
+
+    def handle_buttons(self):
+
+        self.cancel_sto.clicked.connect(self.close)
+        self.compute_stochastic_queue.clicked.connect(self.close)
+        self.cancel.clicked.connect(self.close)
+        self.compute_deterministic.clicked.connect(self.close)
+        self.make_graph.clicked.connect(self.close)
 
 
 def main():
